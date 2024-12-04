de3e33659eeada7c20fe9f0c3df483afefa10305
Martin <martin.bareteau@gmail.com>
2024-12-04 20:38:48 +0100

    effective but no css

---
+++ script.js
@@ -0,0 +1,32 @@
+let unread = document.querySelectorAll("p");
+let total = document.querySelector("h2");
+let notif = unread.length;
+total.innerHTML = notif + " Notifications";
+function reading(index) {
+  if (unread[index].classList.contains("unRead")) {
+    unread[index].classList.remove("unRead");
+    unread[index].classList.add("read");
+    console.log(unread[index]);
+    notif--;
+    total.innerHTML = notif + " Notifications";
+  }
+}
+function allReadFunc() {
+  unread.forEach((p) => {
+    p.classList.remove("unRead");
+    p.classList.add("read");
+    console.log(p);
+    notif = 0;
+    total.innerHTML = notif + " Notifications";
+  });
+}
+const allRead = document.querySelector("button");
+allRead.addEventListener("click", allReadFunc);
+for (let i = 0; i < unread.length; i++) {
+  unread[i].addEventListener("click", function () {
+    reading(i);
+  });
+  unread[i].classList.add("unRead");
+  console.log(unread[i]);
+}
+console.log(notif);
