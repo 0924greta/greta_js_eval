86107adca9ae2c77b0f494ba9370e1e46a88e87a
LilianaAmangoua <amangoua99@gmail.com>
2024-12-05 09:55:15 +0100

    Added fonts and alt descriptions

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 let markAllAsReadButton = document.querySelector("#mark-all-as-read");
 let notifications = document.querySelectorAll("section");
+const pointMarker = document.querySelectorAll(".unread-marker");
 const notificationsCounter = document.querySelector("#notifications-number");
+let counter = null;
 
 markAllAsReadButton.addEventListener('click', () => {
-    if(notificationsCounter.innerText === "3" || notificationsCounter.innerText === "7" ){
-        notifications.forEach((notification) => {
-            notification.classList.remove("unread-notifications");
-            notificationsCounter.innerText = "0";
-            notificationsCounter.classList.add(".notifications-number");
-            
-        })
-    }
-    else if(notificationsCounter.innerText === "0"){
+    if(notificationsCounter.innerText === "0"){
         notifications.forEach((notification) => {
             notification.classList.add("unread-notifications");
             notificationsCounter.innerText = "7";
-            notificationsCounter.classList.add(".notifications-number");
-            
         })
     }
+    else {
+        notifications.forEach((notification) => {
+            notification.classList.remove("unread-notifications");
+            notificationsCounter.innerText = "0";
+        })
+    }
+    counter = Number(notificationsCounter.innerText);
 });
+
+notifications.forEach((notification) => {
+    counter = Number(notificationsCounter.innerText);
+    
+    notification.addEventListener('click', () => {
+        if(notification.classList.contains("unread-notifications")){
+            notification.classList.remove("unread-notifications");
+            counter --;
+        }
+        else {
+            notification.classList.add("unread-notifications");
+            counter ++;
+        }
+        notificationsCounter.innerText = counter;
+    })
+})
