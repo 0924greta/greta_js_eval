73127c723f8a4f4fd98f6d0e47e746949dbb0007
greta <Djymini>
2024-12-05 13:30:25 +0100

    Script js version improve

--- Rendu/script.js
+++ Rendu/script.js
@@ -2,52 +2,44 @@ let notArray = [];
 const divNot = Array.from(document.querySelectorAll('.not'));
 const divNotWithImg = Array.from(document.querySelectorAll('.not-with-img'));
 const divNotWithComment = Array.from(document.querySelectorAll('.not-with-comment'));
-const button = document.querySelector('button');
+const buttonMarkAllRead = document.querySelector('button');
 
-function AddDivInMyArray(array) {
-    for (let index = 0; index < array.length; index++) {
-        notArray.push(array[index]);
-    }
-}
 
-AddDivInMyArray(divNot);
-AddDivInMyArray(divNotWithComment);
-AddDivInMyArray(divNotWithImg);
-
+notArray = notArray.concat(divNot);
+notArray = notArray.concat(divNotWithImg);
+notArray = notArray.concat(divNotWithComment);
 
 function UpdataStatusNotification(){
     let counter = 0;
-    for (let index = 0; index < notArray.length; index++) {
-        if (notArray[index].getAttribute('status') === "unread"){
-            notArray[index].firstElementChild.nextElementSibling.firstElementChild.lastElementChild.style.visibility = "visible"
-            notArray[index].style.backgroundColor= "var(--body-color)";
-            notArray[index].style.boxShadow = "1px 1px 3px var(--shadow-color)";
+    notArray.forEach((div) => {
+        if(div.getAttribute('status') === "unread"){
+            div.firstElementChild.nextElementSibling.firstElementChild.lastElementChild.style.visibility = "visible"
+            div.style.backgroundColor= "var(--body-color)";
+            div.style.boxShadow = "1px 1px 3px var(--shadow-color)";
             counter += 1;
         }
-        else if (notArray[index].getAttribute('status') === "read") {
-            notArray[index].firstElementChild.nextElementSibling.firstElementChild.lastElementChild.style.visibility = "hidden"
-            notArray[index].style.backgroundColor = "var(--section-color)";
-            notArray[index].style.boxShadow = "0px 0px 0px";
+        else if (div.getAttribute('status') === "read") {
+            div.firstElementChild.nextElementSibling.firstElementChild.lastElementChild.style.visibility = "hidden"
+            div.style.backgroundColor = "var(--section-color)";
+            div.style.boxShadow = "0px 0px 0px";
         }
-    }
+    })
     document.querySelector('#not-counter').innerHTML = counter;
 }
+
+
 UpdataStatusNotification();
 
 
 function MarkAllRead(){
-    for (let index = 0; index < notArray.length; index++) {
-        notArray[index].setAttribute('status', 'read');
-    }
+    notArray.forEach(div => div.setAttribute('status', 'read'));
     UpdataStatusNotification();
 }
+buttonMarkAllRead.addEventListener('click', MarkAllRead);
 
-button.addEventListener('click', MarkAllRead);
 
-for (let index = 0; index < notArray.length; index++) {
-    notArray[index].addEventListener('click', (event) => {
-        event.target.setAttribute('status', 'read');
-        UpdataStatusNotification();
-    })
-};
+notArray.forEach(div => div.addEventListener('click', (event) => {
+    event.target.setAttribute('status', 'read');
+    UpdataStatusNotification();
+}));
 
