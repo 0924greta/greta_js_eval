84866d9fd6adea467fb555e482a19ef48da4aab3
greta <Djymini>
2024-12-05 12:38:38 +0100

    Script js version make and exercice is finish

--- Rendu/script.js
+++ Rendu/script.js
@@ -2,6 +2,7 @@ let notArray = [];
 const divNot = Array.from(document.querySelectorAll('.not'));
 const divNotWithImg = Array.from(document.querySelectorAll('.not-with-img'));
 const divNotWithComment = Array.from(document.querySelectorAll('.not-with-comment'));
+const button = document.querySelector('button');
 
 function AddDivInMyArray(array) {
     for (let index = 0; index < array.length; index++) {
@@ -13,14 +14,40 @@ AddDivInMyArray(divNot);
 AddDivInMyArray(divNotWithComment);
 AddDivInMyArray(divNotWithImg);
 
-console.log(notArray[0].firstElementChild.nextElementSibling.firstElementChild.lastElementChild);
 
 function UpdataStatusNotification(){
+    let counter = 0;
     for (let index = 0; index < notArray.length; index++) {
-        if (notArray[index].getAttribute('status') = "unread"){
+        if (notArray[index].getAttribute('status') === "unread"){
             notArray[index].firstElementChild.nextElementSibling.firstElementChild.lastElementChild.style.visibility = "visible"
-            notArray[index].backgroundColor= "var(--body-color)";
+            notArray[index].style.backgroundColor= "var(--body-color)";
+            notArray[index].style.boxShadow = "1px 1px 3px var(--shadow-color)";
+            counter += 1;
         }
-        
+        else if (notArray[index].getAttribute('status') === "read") {
+            notArray[index].firstElementChild.nextElementSibling.firstElementChild.lastElementChild.style.visibility = "hidden"
+            notArray[index].style.backgroundColor = "var(--section-color)";
+            notArray[index].style.boxShadow = "0px 0px 0px";
+        }
     }
+    document.querySelector('#not-counter').innerHTML = counter;
 }
+UpdataStatusNotification();
+
+
+function MarkAllRead(){
+    for (let index = 0; index < notArray.length; index++) {
+        notArray[index].setAttribute('status', 'read');
+    }
+    UpdataStatusNotification();
+}
+
+button.addEventListener('click', MarkAllRead);
+
+for (let index = 0; index < notArray.length; index++) {
+    notArray[index].addEventListener('click', (event) => {
+        event.target.setAttribute('status', 'read');
+        UpdataStatusNotification();
+    })
+};
+
