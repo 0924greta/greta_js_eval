7dd8294f7c96bcf7aa20f71208b9189397e4b77c
pilou <p-l.b@hotmail.com>
2024-12-05 12:24:30 +0100

    proposed modification with callback

--- script.js
+++ script.js
@@ -28,19 +28,24 @@ let unreadBox = document.querySelectorAll(".unread");
 console.log(unreadBox);
 
 unreadBox.forEach((el, idx) =>
-  el.addEventListener("click", () => {
-    //change class unread to read
-    el.classList.replace("unread", "read");
-    unreadBox = document.querySelectorAll(".unread");
-    //notification count reduce
-    minusNotif();
-    //remove red-point class to make point disapear
-    let nbp = document.querySelector(`.pt${idx}`);
-    nbp.classList.remove("red-point");
+  el.addEventListener("click", (event) => {
+    handleUnread(event, el, idx)
   })
 );
 
 function minusNotif() {
   const count = document.querySelector("#notification-count");
   count.innerText = `${unreadBox.length}`;
+}
+
+
+function handleUnread(event, el, idx) {
+  //change class unread to read
+  el.classList.replace("unread", "read");
+  unreadBox = document.querySelectorAll(".unread");
+  //notification count reduce
+  minusNotif();
+  //remove red-point class to make point disapear
+  let nbp = document.querySelector(`.pt${idx}`);
+  nbp.classList.remove("red-point");
 }
