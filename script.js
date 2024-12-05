37ac9a20172e48dfdd2bc4ff76d0e2f43a4a284f
Gabrielloop <gabriel.henin@gmail.com>
2024-12-05 13:56:19 +0100

    Refactor index.html, script.js & style.css

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// function nbUnreadNotification() {
-
-// Notification unread
+// counter of notifications
 let notifCounter = 0;
 
+// function to update notifications counter (in header)
 function updateNotifCounter() {
   const unreadNotificationArray = document.querySelectorAll(".unread");
   notifCounter = unreadNotificationArray.length;
@@ -10,31 +9,42 @@ function updateNotifCounter() {
   targetNotifCounter.innerText = notifCounter;
   return notifCounter;
 }
+
+// function to add listerner to the "mark all as read" link
 function markAllReadListener() {
+  // 2 targets with front update :
   const unreadNotificationArray = document.querySelectorAll(".unread");
   const unreadNotificationBadge = document.querySelectorAll(".badge-unread");
-  const markAllReadBtn = document.querySelector(".mark-read-btn");
+  // DOM location of link
+  const markAllReadBtn = document.querySelector(".mark-all-read-btn");
 
+  // Add a listerner to DOM location of link
   markAllReadBtn.addEventListener("click", () => {
     unreadNotificationArray.forEach((element, index) => {
       element.classList.remove("unread");
       unreadNotificationBadge[index].classList.remove("badge-unread");
     });
+    // Auto-update the notification counter
     updateNotifCounter();
   });
 }
-markAllReadListener();
+markAllReadListener(); // Always active listener
 
+// function to add listerner to each notifications "unread"
 function unreadListenerOnNotification() {
+  // 2 targets with front update :
   const unreadNotificationArray = document.querySelectorAll(".unread");
   const unreadNotificationBadge = document.querySelectorAll(".badge-unread");
 
+  // For each notifications "unread" :
   unreadNotificationArray.forEach((element, index) => {
+    // Add listener to this element
     element.addEventListener("click", () => {
       element.classList.remove("unread");
       unreadNotificationBadge[index].classList.remove("badge-unread");
+      // Auto-update the notification counter
       updateNotifCounter();
     });
   });
 }
-unreadListenerOnNotification();
+unreadListenerOnNotification(); // Always active listener
