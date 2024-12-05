71bbeaff01fb990b9be87cabee0f152af2583fd5
LilianaAmangoua <amangoua99@gmail.com>
2024-12-05 12:35:41 +0100

    Added responsive for tablets

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const notificationsMarker = document.querySelectorAll(".unread-marker");
 let counter = null;
 
 /* Fonction pour le point rouge*/
-notifications.forEach((notification) => {
-    notifications = Array.from(notifications);
-    const index = notifications.indexOf(notification);
+notifications = Array.from(notifications);
 
+notifications.forEach((notification) => {
+    const index = notifications.indexOf(notification);
     if(!notification.classList.contains("unread-notifications")){
         notificationsMarker[index].style.display = "none";
     }
@@ -19,7 +19,6 @@ notifications.forEach((notification) => {
 markAllAsReadButton.addEventListener('click', () => {
     if(notificationsCounter.innerText === "0"){
         notifications.forEach((notification) => {
-            notifications = Array.from(notifications);
             const index = notifications.indexOf(notification);
 
             notification.classList.add("unread-notifications");
@@ -29,7 +28,6 @@ markAllAsReadButton.addEventListener('click', () => {
     }
     else {
         notifications.forEach((notification) => {
-            notifications = Array.from(notifications);
             const index = notifications.indexOf(notification);
 
             notification.classList.remove("unread-notifications");
@@ -42,9 +40,7 @@ markAllAsReadButton.addEventListener('click', () => {
 
 /*Fonction pour mettre chaque notification en lu ou non-lu*/
 notifications.forEach((notification) => {
-    notifications = Array.from(notifications);
     const index = notifications.indexOf(notification);
-
     counter = Number(notificationsCounter.innerText);
     
     notification.addEventListener('click', () => {
