be89a2e3a882e474fd1ccb2ca2a3dcc71ed94df3
Kevin8637 <kevin.lar@live.fr>
2024-12-05 10:20:01 +0100

    Add red point and a disfonctionned counter

--- my-page/script.js
+++ my-page/script.js
@@ -1,3 +1,4 @@
+//Fonction qui permet de changer le background color des div (message) en blanc quand on clique sur chacune des div
 function changeBackgroundColor() {
     this.style.backgroundColor = 'white'; 
 }
@@ -6,10 +7,7 @@ divs.forEach(div => {
 div.addEventListener('click', changeBackgroundColor);
 });
 
-function readAll() {
-    divs.style.backgroundColor = "white";
-}
-
+//Fonction qui permet de changer le background color de toutes les div (message) en blanc quand on clique sur le bouton 'Mark all as read'
 function readAll() {
     divs.forEach(div => {
         div.style.backgroundColor = 'white';
@@ -18,3 +16,17 @@ function readAll() {
 
 const btn = document.querySelector('button');
 btn.addEventListener("click", readAll);
+
+//Fonction qui met en place un compteur pour voir le nombre de notifications non lues
+function countNotificationsNotRead (){
+    let count=0;
+    divs.forEach(div =>{
+        let color = getComputedStyle(div).backgroundColor;
+        if (color === 'hsl(211, 68%, 94%)') {
+            count++;
+        }
+    })
+    document.querySelector(".numberOfNotifications").innerText = count;
+}
+
+countNotificationsNotRead();
